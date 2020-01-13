4d307601c82f455ec8487ddd5dab06f598605318
jdegaris <j.a.degaris@gmail.com>
2020-01-12 23:52:40 -0500

    small changes

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -5,8 +5,8 @@ const Instructions = () => {
         <Fragment>
             <p><span className="bang">Before beginning please make sure to:</span></p>
             <ol id="instructions">
-                <li>Close all other tabs that may be open</li>
-                <li>Save all work and close all other open programs</li>
+                <li>Close any other tabs that may be open in your browser</li>
+                <li>Save all work and close any other open programs on your computer</li>
                 <li>Read each step entirely before taking action.</li>
             </ol>
         </Fragment>
